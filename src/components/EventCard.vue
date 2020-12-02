2f34a7f420f7b30995d6665532eaa5fd21db8cb1
othman othman <othman.hesabe@gmail.com>
2020-12-02 13:57:21 +0300

    commit lesson -8

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,8 +6,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <BaseIcon name="users"> {{ event.attendees.length }} attending </BaseIcon>
     </div>
   </router-link>
 </template>
@@ -18,7 +17,7 @@ export default {
     return {
       event: {
         id: 1,
-        title: "Park Cleanup !++.!",
+        title: "Park Cleanup",
         date: "Tues Dec 1, 2020",
         time: "4:00",
         attendees: [
